08a6783d378de90bf37455803f4a5c7b63a8dc73
adriamiralozano <90398377+adriamiralozano@users.noreply.github.com>
2021-09-30 10:33:53 +0200

    Ajustar función update

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,6 +13,8 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
