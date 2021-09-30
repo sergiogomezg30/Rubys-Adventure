c0080b2481a5ecfdf5f35ba2e369ccafe0c74397
adriamiralozano <90398377+adriamiralozano@users.noreply.github.com>
2021-09-30 12:18:32 +0200

    movimiento mas lento

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,8 +7,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+
     }
 
     // Update is called once per frame
@@ -19,8 +18,8 @@ public class RubyController : MonoBehaviour
 
         Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.1f * vertical * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
