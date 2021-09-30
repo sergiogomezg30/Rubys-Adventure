84e044ba6262d67cfe509100d06dc8fc1e9bbc21
adriamiralozano <90398377+adriamiralozano@users.noreply.github.com>
2021-09-30 11:59:40 +0200

    Cambiar el script movimiento teclado

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,8 +13,10 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
 }
