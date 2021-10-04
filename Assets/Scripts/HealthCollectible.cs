bc2d9551de869fabc8a3bd6e0305edb91397c2d6
sergiogomezg30 <al386174@uji.es>
2021-10-04 10:56:24 +0200

    Give Ruby Health

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
    void OnTriggerEnter2D(Collider2D other)
-   {
-       Debug.Log("Object that entered the trigger : " + other);
-   }
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
 }
