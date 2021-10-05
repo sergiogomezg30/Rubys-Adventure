05d50f790844ecc1b35ad46212ef509f6e0d8e67
sergiogomezg30 <al386174@uji.es>
2021-10-05 13:20:07 +0200

    Launch the Projectile

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -21,6 +21,8 @@ public class RubyController : MonoBehaviour
     
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
+
+    public GameObject projectilePrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -80,5 +82,15 @@ public class RubyController : MonoBehaviour
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
