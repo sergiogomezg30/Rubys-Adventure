12c1cf4777c5a19efb09907071ba180315874592
sergiogomezg30 <al386174@uji.es>
2021-10-05 13:15:48 +0200

    The Physics System

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+    
     void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Launch(Vector2 direction, float force)
     {
-        
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //we also add a debug log to know what the projectile touch
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        Destroy(gameObject);
     }
 }
