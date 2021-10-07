059f3e729130f76d235c94aa1f458cb62401c2e8
adriamiralozano <90398377+adriamiralozano@users.noreply.github.com>
2021-10-07 16:52:48 +0200

    Fixing the Smoke Movement 2

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -78,6 +78,7 @@ public class EnemyController : MonoBehaviour
     {
         broken = false;
         rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
         smokeEffect.Stop();
     }
 }
